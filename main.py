5d63efc94a3e0b224aeb87d1df273f97af953e19
Stick <joanfabianliceo6c@gmail.com>
2024-12-29 22:49:32 -0400

    cambios a main player

--- main.py
+++ main.py
@@ -3,62 +3,6 @@ import math
 from data import *
 
 
-class Cartas(Entity) : 
-    def __init__(self, parent, name = "Entity", position=Vector2(), scale=Vector2(1, 1), rotation = 0, origin=Vector2(0, 0)):
-        super().__init__(parent, name, position, scale, rotation, origin)
-        
-        self.mouse = Collider(parent=self.parent, name="Mouse", scale=Vector2(0.1, 0.1),origin=Vector2(0, 0), color=RED, use_basis_model=True)
-        
-        self.use_y_index = False
-        
-        self.model = Entity(parent=self, position=Vector2(0,0), scale=Vector2(1.4, 2), origin=Vector2(1.4/2,1))
-        self.model.use_y_index = False
-        self.model_shadown = ModelBasic(parent=self.model, position=Vector2(5,5), scale=Vector2(1, 1), color=Color(0, 0, 0, 50), origin=Vector2(1.4/2,1))
-        self.model_base = ModelBasic(parent=self.model, position=Vector2(0,0), scale=Vector2(1, 1), color=Color(150, 60, 100, 255), origin=Vector2(1.4/2,1))
-        self.model_base_scale : Vector2
-        self.model_move : Vector2
-        
-        self.is_select = False
-        self.mouse_enter_position : Vector2 = Vector2()
-        self.mouse_position :Vector2 = Vector2()
-        self.origin = Vector2(0.5, 0)
-        
-        self.collider = Collider(
-            parent=self, position=Vector2(0,0), name="Collider", 
-            scale=Vector2(1.4, 2), use_basis_model=False, 
-            how_collider="BOX", color=Color(0, 0, 255, 20), origin=Vector2(1.4/2,1)
-        )
-    def Update(self, dt):
-        
-        
-        self.mouse.position = get_mouse_position()
-                
-        if self.collider.IsCollider() and is_mouse_button_pressed(0):
-            self.is_select = not self.is_select
-            self.mouse_enter_position = self.mouse.position
-            self.mouse_position = vector2_subtract(self.mouse.position, self.position)
-            self.model_base_scale = vector2_add(self.scale, Vector2(0.1, 0.1))
-            self.model_move = Vector2(10, 10)
-            
-        if self.is_select and is_mouse_button_down(0): 
-            
-            
-            self.model.rotation = lerp(self.model.rotation, ((get_mouse_delta().x + get_mouse_delta().y)/2)*2, .05)
-            self.position.x = lerp(self.position.x, self.mouse.position.x - self.mouse_position.x, 0.2)
-            self.position.y = lerp(self.position.y, self.mouse.position.y - self.mouse_position.y, 0.2)
-            
-        else : 
-            self.model_base_scale = Vector2(1,1)
-            self.model_move = Vector2(2, 2)
-            self.model.rotation = lerp(self.model.rotation, 0, .1)
-            self.is_select = False
-        
-        self.model_base.scale = vector2_lerp(self.model_base.scale, self.model_base_scale, 0.5)
-        self.model_base.position = vector2_lerp(self.model_base.position,Vector2(self.model_move.x*-1, self.model_move.y*-1), 0.5)
-        #self.model_shadown.scale = vector2_lerp(self.model_shadown.scale, self.model_base_scale, 0.5)
-        self.model_shadown.position = vector2_lerp(self.model_shadown.position, self.model_move, 0.5)
-        
-        return super().Update(dt)
 
 class Game(Engine):
     def __init__(self, window_size_x, window_size_y, name, use_y_index = False):
@@ -66,14 +10,30 @@ class Game(Engine):
         set_target_fps(60)
         
         self.scena = Scena("Cartas")
-        self.scena.use_y_index = True
-        Cartas(parent=self.scena, position=Vector2(400, 400))
+        self.scena.use_y_index = False
+        
+        self.tilemap = TileMap(parent=self.scena, position=Vector2(0, 0),  texture="grass.png", tile_size=Vector2(8, 8))
+        self.tilemap.scale = Vector2(4, 4)
+        for x in range(-5, 5):
+            self.tilemap.AddTile(Vector2(x, 0), Vector2(0, 0))
+        
+        self.model = ModelBasic(parent=self.scena, scale=Vector2(4, 4), size=Vector2(8, 8), color=Color(255, 255, 225, 50))
         
     def Update(self, dt):
-
-        if is_key_pressed(KEY_A) : 
-            Cartas(parent=self.scena, position=get_mouse_position())
         
+
+        target = get_mouse_position()
+        step = 8 * 4
+        x = int(target.x / step) * step
+        y = int(target.y / step) * step
+        self.model.position = Vector2(x, y)
+        target = Vector2(int(x / step), int(y / step))
+        
+        if is_mouse_button_down(0) : 
+            self.tilemap.AddTile(target, Vector2(0, 0))
+        if is_mouse_button_down(1) :
+            self.tilemap.RemoveTile(target)
+    
         return super().Update(dt)
     
     def Draw(self):
